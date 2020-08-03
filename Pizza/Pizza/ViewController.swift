f9ff43247d5cd235bfce4bdef23968822faa3654
jmei16 <67976097+jmei16@users.noreply.github.com>
2020-08-03 18:05:06 -0500

    Add files via upload

---
+++ Pizza/Pizza/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pizza
+//
+//  Created by Jia Ming Mei on 8/3/20.
+//  Copyright © 2020 Jia Ming Mei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
